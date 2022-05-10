da5cc2a1c8ac38d67a03c061f70ee435ff1aa7d0
Krhuhun <72083150+jorgelisboa@users.noreply.github.com>
2022-05-10 20:12:09 -0300

    Forget about OOP
    Changed because some people from our group doesn't know OOP.

--- Main.py
+++ Main.py
@@ -4,12 +4,53 @@ import Data as db
 #Variables
 username = None
 password = None 
+consumedFood = [[],[]]
 
 class TelaLogin:
 
+    ###IMC###
+    def calcImc(altura, peso):
 
+        imcValue = peso / (altura ** altura)
+        return imcValue
+
+    def findCategory(imc):
+        if imc < 18.5:
+            return "MAGREZA"
+        elif imc >= 18.5 and imc <= 24.9:
+            return "NORMAL"
+        elif imc >= 25 and imc <= 29.9:
+            return "SOBREPESO"
+        elif imc >= 30 and imc <= 39.9:
+            return "OBESIDADE"
+        elif imc > 40:
+            return "OBESIDADE GRAVE"
+    
+    ###CALORIES### 
+    def returnFoodList():
+        return consumedFood
+    
+    def addFood(foodName, calories):
+        consumedFood[0].append(foodName)
+        consumedFood[1].append(calories)
+
+    def countTotalCalories():
+        count = 0
+        totalCalories = 0
+        while count <= len(consumedFood)+1:
+            totalCalories += consumedFood[1][count]
+            count = count+1
+            
+    def listAllEatenFood():
+        count = 0
+        while count <= len(consumedFood)+1:
+            print(str(consumedFood[0][count]) + " -> " + str(consumedFood[1][count]))
+            count = count+1
+
+
+
+    #Layout
     def __init__(self):
-        #Layout
         def createLayout():
 
             layout = [
@@ -28,8 +69,9 @@ class TelaLogin:
         password = self.values['password']
         print('nome: '+username+' password: '+password)
 
-    def checkLogin():
+    #def checkLogin():
         #DB class calling
+
 
 tela = TelaLogin()
 tela.Iniciar()
